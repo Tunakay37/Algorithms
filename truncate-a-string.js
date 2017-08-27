98dd5b41a9762d894e3769881766bf295f568b0b
Samuel Akintunde <samakintunde37@gmail.com>
2017-08-27 22:00:52 +0100

    Create truncate-a-string.js

---
+++ truncate-a-string.js
@@ -0,0 +1,17 @@
+function truncateString(str, num) {
+  
+  //if str is longer than the given maximum string length, 'num'
+  if(str.length > num && num > 3) {
+    var str1 = str.slice(0, num-3);
+    return str3 + '...';
+  }
+  //if the given maximum string length 'num' is less than or equal to 3
+  else if(num <= 3) {
+    var str2 = str.slice(0, num);
+    return str2 + '...';
+  }
+  //otherwise, just return string
+  else {
+    return str;
+  }
+}

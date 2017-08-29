b352658398d28fdbd5faa643dc261ccfde538bbf
Samuel Akintunde <samakintunde37@gmail.com>
2017-08-29 11:21:41 +0100

    Update insert-number-into-array-ordered.js

--- insert-number-into-array-ordered.js
+++ insert-number-into-array-ordered.js
@@ -1,10 +1,11 @@
 function getIndexToIns(arr, num) {
- 
-  var times = arr.length; 
+  
+  var times = arr.length;
   var count = 0; 
-  for (i=0;i<times;i++){
-    if(num>arr[i]){
-      count++;} 
+  for (i=0; i < times; i++) {
+    if(num > arr[i]) {
+      count++;
     } 
-  return count; 
+  }
+    return count;
 }
